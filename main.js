9cc7692df28c25641caea26736370bf18416bf3d
ruhidnovruzov <ruhidnovruzov2@gmial.com>
2023-11-27 19:41:25 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// TASK-1
+
+// function convertNumberToString(arr){
+//   var convertedArray= arr.map((element)=>{
+//     return  element.toString()
+//   } )
+
+//   return convertedArray
+// }
+
+// var inputArray= [2, 'aasdcasfc', 54, 'eqwfwe', 8]
+// var result=convertNumberToString(inputArray)
+
+// console.log(result)
+
+// TASK-2
+
+// function findType(arr){
+//   var findedType= arr.map((element)=>{
+//     return  typeof(element)
+//   } )
+
+//   return findedType
+// }
+
+// var inputArray= [2, 'aasdcasfc', 54, 'eqwfwe', 8, null, undefined, [2, ['asfdsds']]]
+// var result=findType(inputArray)
+// console.log(inputArray)
+// console.log(result)
+
+// TASK-3
+
+// function calcSum(arr) {
+//   var sum = arr.reduce((acc, current) => acc + current, 0)
+//   return sum
+// }
+
+// var array = [1, 2, 3, 4, 5]
+// const result = calcSum(array)
+
+// console.log(result)
+
+// TASK-4
+
+// function filterFourLetterWords(arr) {
+
+//   var fourLetterWords = arr.filter(function(word) {
+//     return word.length === 4
+//   })
+//     return fourLetterWords
+// }
+
+
+// var wordsArray = ["Ryan", "Kieran", "Jason", "Matt"]
+// var resultArray = filterFourLetterWords(wordsArray)
+
+// console.log(resultArray)
+
